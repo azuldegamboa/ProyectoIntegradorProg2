e5d54b86082e92c40b92cef8ac60720630c50cdf
m-lombardi <56735001+m-lombardi@users.noreply.github.com>
2021-09-18 11:33:33 -0700

    cosas

--- proyectoint/routes/posts.js
+++ proyectoint/routes/posts.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 const controller = require('../controllers/postController');
 
-router.get('/', controller.post);
+router.get('/add', controller.agregarPost);
 
-router.get('/detalle/:id', controller.agregarPost);
-
+router.get('/detalle/:id', controller.post);
 
 
 
--- proyectoint/routes/users.js
+++ proyectoint/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const controller = require('../controllers/userController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.get('/details/:id', controller.detalleUsuario);
+
+router.get('/edit', controller.editarPerfil);
+
+router.get('/login', controller.login);
+
+router.get('/profile', controller.miPerfil);
+
+router.get('/register', controller.registracion);
+
 
 module.exports = router;
