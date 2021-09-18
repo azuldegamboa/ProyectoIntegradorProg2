09cd936e28dcf11335612ef237ae844f0c0e0be9
larafernandezo <56734918+larafernandezo@users.noreply.github.com>
2021-09-18 11:49:12 -0300

    formularios

--- proyectoint/data/post.js
+++ proyectoint/data/post.js
@@ -1,73 +1,73 @@
 let post={
     lista:[{
-        id: 0,
+        id: 1,
         foto:"",
         descripcion: "",
         fecha_creacion: "23/09",
         usuario_id: 1
     },
     {
-        id: 0,
+        id: 2,
+        foto:"",
+        descripcion: "",
+        fecha_creacion: "23/09",
+        usuario_id: 2
+    },
+    {
+        id: 3,
+        foto:"",
+        descripcion: "",
+        fecha_creacion: "23/09",
+        usuario_id: 3
+    },
+    {
+        id: 4,
+        foto:"",
+        descripcion: "",
+        fecha_creacion: "23/09",
+        usuario_id: 4
+    },
+    {
+        id: 5,
+        foto:"",
+        descripcion: "",
+        fecha_creacion: "23/09",
+        usuario_id: 5
+    },
+    {
+        id: 6,
         foto:"",
         descripcion: "",
         fecha_creacion: "23/09",
         usuario_id: 1
     },
     {
-        id: 0,
+        id: 7,
         foto:"",
         descripcion: "",
         fecha_creacion: "23/09",
-        usuario_id: 1
+        usuario_id: 2
     },
     {
-        id: 0,
+        id: 8,
         foto:"",
         descripcion: "",
         fecha_creacion: "23/09",
-        usuario_id: 1
+        usuario_id: 3
     },
     {
-        id: 0,
+        id: 9,
         foto:"",
         descripcion: "",
         fecha_creacion: "23/09",
-        usuario_id: 1
+        usuario_id: 4
     },
     {
-        id: 0,
+        id: 10,
         foto:"",
         descripcion: "",
         fecha_creacion: "23/09",
-        usuario_id: 1
-    },
-    {
-        id: 0,
-        foto:"",
-        descripcion: "",
-        fecha_creacion: "23/09",
-        usuario_id: 1
-    },
-    {
-        id: 0,
-        foto:"",
-        descripcion: "",
-        fecha_creacion: "23/09",
-        usuario_id: 1
-    },
-    {
-        id: 0,
-        foto:"",
-        descripcion: "",
-        fecha_creacion: "23/09",
-        usuario_id: 1
-    },
-    {
-        id: 0,
-        foto:"",
-        descripcion: "",
-        fecha_creacion: "23/09",
-        usuario_id: 1
+        usuario_id: 5
     }]
 }
 
