9d3971465cc54c9ed1b0973ccda6f425bd13c633
larafernandezo <56734918+larafernandezo@users.noreply.github.com>
2021-09-26 10:18:32 -0300

    lari2

--- proyectoint/controllers/userController.js
+++ proyectoint/controllers/userController.js
@@ -8,7 +8,7 @@ let controller={
        for (let i = 0; i < users.lista.length; i++) {
         const element = users.lista[i];
         if (element.id==req.params.id) {//el id de esta publicacion es igual al id del parametro que esta en el link, si alguuno de los elemento dentro de la lista de post tiene el mismo id que el que yo busque en la ruta que me lo traiga
-          res.render('detalleUsuario',{user:element, post: post.lista, comentarios: comentario.lista});
+          res.render('detalleUsuario',{user:element, posts: post.lista, comentarios: comentario.lista});
         }
       }
 
@@ -20,13 +20,7 @@ let controller={
         res.render('login');
       },
     miPerfil: function(req, res, next) { //req= request res = response
-      for (let i = 0; i < users.lista.length; i++) {
-        const element = users.lista[i];
-        if (element.id==req.params.id) {//el id de esta publicacion es igual al id del parametro que esta en el link, si alguuno de los elemento dentro de la lista de post tiene el mismo id que el que yo busque en la ruta que me lo traiga
-          res.render('miPerfil',{user:element, post: post.lista, comentarios: comentario.lista});
-        }
-      }
-
+          res.render('miPerfil',{user:users.lista[0], posts: post.lista, comentarios: comentario.lista})
       },
     registracion: function(req, res, next) { //req= request res = response
         res.render('registracion');
